23968bef33aafd8ced7f9323b41ad137e476fe3a
dterng928 <bhatarasiri_cud@hotmail.com>
2022-02-19 22:58:12 +0700

    add and use variable from config file (application.properties)

--- src/main/java/com/example/hello/users/UserGateway.java
+++ src/main/java/com/example/hello/users/UserGateway.java
@@ -1,6 +1,7 @@
 package com.example.hello.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,9 +11,15 @@ public class UserGateway {
     //RestTemplate เป็นของ spring boot เอาไว้ยิง api : ยิง api = gateway
     @Autowired
     private RestTemplate restTemplate;
+    private String baseUrl;
+
+    @Autowired
+    public UserGateway(@Value("${base_url}") String baseUrl){
+        this.baseUrl = baseUrl;
+    }
 
     public UserResponse getUserById(int id){
-        UserResponse response = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users/"+id, UserResponse.class);
+        UserResponse response = restTemplate.getForObject(this.baseUrl+"users/"+id, UserResponse.class);
         return response;
     }
 }
