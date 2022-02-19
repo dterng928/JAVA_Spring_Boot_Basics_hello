235442e61378d4c47404048a2e76b6fd0c383b7d
dterng928 <bhatarasiri_cud@hotmail.com>
2022-02-19 23:20:03 +0700

    transaction handling -> 1 time / multiple times

---
+++ src/main/java/com/example/hello/tx/DemoTxService.java
@@ -0,0 +1,26 @@
+package com.example.hello.tx;
+
+import com.example.hello.User;
+import com.example.hello.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+// @Transactional ครอบที่ class ทำให้ทุก method มี @Transactional หรือเราสามารถระบุทีละ method ก็ได้
+public class DemoTxService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void callRepository() {
+        User user1 = new User(1, "User 1");
+        User user2 = new User(2, "User 2");
+        User user3 = new User(3, "User 3");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+    }
+}
