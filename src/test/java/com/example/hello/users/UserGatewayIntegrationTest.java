b0793ce4dafa4fce12499d8fef6333705f207f71
dterng928 <bhatarasiri_cud@hotmail.com>
2022-02-20 10:54:45 +0700

    add integration test

---
+++ src/test/java/com/example/hello/users/UserGatewayIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.example.hello.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Integration test (for test connection)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserGatewayIntegrationTest {
+
+    @Autowired
+    private UserGateway userGateway;
+
+    // เนื่องจาก integration test เป็นการ run จริง เราจึงควรตั้ง schedule เพื่อไม่ให้กระทบส่วนอื่น
+    // ผลกระทบเกิดจากปลายทางสามารถเปลี่ยน result หรือปัญหาด้าน network -> generate fake server (
+    @Test
+    void getUserById() {
+        // Act
+        UserResponse result = userGateway.getUserById(1);
+        // Assert
+        assertEquals(1, result.getId());
+        assertEquals("Leanne Graham", result.getName());
+    }
+}
